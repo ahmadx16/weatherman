5b557b64cfeb106db4bc7f75aa65c9ffffac31f9
ahmadx16 <ahmadx16@hotmail.com>
2020-07-24 07:30:35 +0500

    initial year report

--- weatherman.py
+++ weatherman.py
@@ -7,6 +7,9 @@ import sys
 
 path = "./weatherfiles/"
 
+# main data structure
+year_dic = {}
+
 
 def string_to_date(s_date):
     year, month, day = re.split("-", s_date)
@@ -54,29 +57,62 @@ def handle_xlsx(f_name):
 
 
 def printer(year_dic):
-    for k, v in year_dic.items():
-        print(k)
-    
+    print(len(year_dic[2012][1][0]))
+
 
 def handle_sys_argv(sys_argv):
-    argv_dic={}
+    argv_dic = {}
+    if len(sys_argv) > 1:
+        argv_dic["path"] = sys_argv[1]
 
-    argv_dic["path"] = sys_argv[1]
-
-    if "-e" in sys_argv:
-        argv_dic["-e"] = sys_argv[sys_argv.index("-e")+1]
-    if "-a" in sys_argv:
-        argv_dic["-a"] = sys_argv[sys_argv.index("-a")+1]
-    if "-c" in sys_argv:
-        argv_dic["-c"] = sys_argv[sys_argv.index("-c")+1]
-
+        if "-e" in sys_argv:
+            argv_dic["-e"] = sys_argv[sys_argv.index("-e")+1]
+        if "-a" in sys_argv:
+            argv_dic["-a"] = sys_argv[sys_argv.index("-a")+1]
+        if "-c" in sys_argv:
+            argv_dic["-c"] = sys_argv[sys_argv.index("-c")+1]
 
     return argv_dic
 
 
-year_dic = {}
+def year_report(year):
+    year_data = year_dic[year]
+
+    max_temp = -273
+    min_temp = 1000
+    min_humid = 100
+    for k, month in year_data.items():
+        for day in month:
+            temp = day["Max TemperatureC"]
+            if temp != '':
+                temp = int(temp)
+                if temp > max_temp:
+                    max_temp = temp
+            temp = day["Min TemperatureC"]
+            if temp != '':
+                temp = int(temp)
+                if temp < min_temp:
+                    min_temp = temp
+            humid = day["Min Humidity"]
+            if humid != '':
+                humid = int(humid)
+                if humid < min_humid:
+                    min_humid = humid
+    print(max_temp)
+    print(min_temp)
+    print(min_humid)
+
+
+def month_report(year, month):
+    pass
+
+
+def month_chart(year, month):
+    pass
+
+
 if __name__ == "__main__":
-   
+
     argv_dic = handle_sys_argv(sys.argv)
 
     with zipfile.ZipFile("./weatherfiles.zip", 'r') as zip_ref:
@@ -88,7 +124,6 @@ if __name__ == "__main__":
             if year in year_dic:
                 year_dic[year][month] = dates
             else:
-                # print(year,month)
                 year_dic[year] = dict()
                 year_dic[year][month] = dates
 
@@ -98,4 +133,14 @@ if __name__ == "__main__":
             handle_xlsx(f_name)
             # break
 
-    # printer(year_dic)
+    if "-e" in argv_dic:
+        year = int(argv_dic["-e"])
+        year_report(year)
+    if "-a" in argv_dic:
+        y_m = argv_dic["-a"]
+        year, month = re.split("/", y_m)
+        month_report(int(year), int(month))
+    if "-c" in argv_dic:
+        y_m = argv_dic["-c"]
+        year, month = re.split("/", y_m)
+        month_chart(int(year), int(month))
